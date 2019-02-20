44042fbf081c32266cc41a2fbedbc294b1eedf19
hs <hsmok81@gmail.com>
2019-02-20 18:36:48 +0700

    Change AddVendor to AddUser

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Dashboard from '../screens/Dashboard';
-import AddVendor from '../screens/vendor/AddVendor';
+import AddUser from '../screens/user/AddUser';
 
 export default class Navigation extends Component{
     render() {
         return (
             <Switch>
                 <Route exact path='/' component={Dashboard}/>
-                <Route path='/addVendor' component={AddVendor}/>
+                <Route path='/addUser' component={AddUser}/>
                 {/* <Route path='/users' component={users}/>
                 <Route path='/teacher' component={teacher}/>
                 <Route path='/courses' component={courses}/>
